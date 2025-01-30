c123661c93d1a7788f8a9edf963862db4821e5d6
ai_8삼성 인정배 <43261890+sdfasdsf@users.noreply.github.com>
2025-01-31 00:53:15 +0900

    test 원인 html staic 경로 문제

--- Main/urls.py
+++ Main/urls.py
@@ -4,7 +4,9 @@ from . import views
 
 
 urlpatterns = [
+    path('config.js', views.config_js, name='config_js'),
     path('', views.Main.as_view(), name="Main"),
     path('movie/<int:movie_id>/', views.Movie.as_view(), name="movie"),
+
 
 ]
